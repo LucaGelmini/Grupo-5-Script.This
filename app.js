3f1f0c101af403112827f0f3428dd3731190eb5b
Luca <lucagelmini@github.com>
2022-02-11 19:03:00 -0300

    Copio la estructura de los formularios del proyecto individual , cambio un par de reglas de css y muevo div del carrusel para que quede mejor

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const publicPath = path.resolve(__dirname,'./public')
 app.use(express.static(publicPath))
 
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor encendido !');
-})
-
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/index.html'))
 })
+
+app.get('/register',(req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+app.get('/login',(req, res)=> {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.listen(3001, ()=>{
+    console.log('Servidor encendido !');
+})
+
