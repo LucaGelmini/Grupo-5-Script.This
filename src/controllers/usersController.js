5a299e12a6d83a8e65a58d15ea84ab69cef89a43
Luca <lucagelmini@github.com>
2022-04-13 03:56:58 -0300

    trabajo en la vista de perfil de usuario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,17 +2,19 @@
 const {validationResult} = require('express-validator');
 const Users = require('../models/Users');
 const bcrypt = require('bcryptjs/dist/bcrypt');
+const session = require('express-session');
  
 
 const usersController = {
     loginView: (req, res)=>{
         res.render('login')
+        
     },
     registerView: (req, res)=>{
          res.render('register')
     },
     profile: (req, res)=>{
-         res.render('profile')
+         res.render('profile', {})
     },
     logout: (req,res)=>{
         res.clearCookie('userEmail');
@@ -61,12 +63,12 @@ const usersController = {
         const validation = validationResult(req);
         let userInDB = Users.findByField('email',req.body.email)
       
-        if(validation.errors.length > 0){
+        if(validation.errors.length > 0){ //en el caso de errores
 			res.render('login', {
 				 errors:validation.mapped(),
 				 oldData: req.body});
 
-		} else if (!userInDB) {
+		} else if (!userInDB) { //en el caso de que user no exista
 			res.render('login',{
 				errors: {
 					email:{
@@ -74,28 +76,29 @@ const usersController = {
 					}
 				}
 			})
-		};
-
-        if (userInDB){
-            let passwordOk = bcrypt.compareSync(req.body.password, userInDB.password);
-            if(passwordOk){
-                delete userInDB.password;
-                req.session.logedUser = userInDB;
-                console.log(req.session.logedUser);
-                if(req.body.recordame){
-                    res.cookie('userEmail', req.body.email, {maxAge: (1000*60)*60})
+		} else { // No hay errores y ya verificamos que el usuario exista. Comparamos la password.
+            bcrypt.compare(req.body.password, userInDB.password)
+            .then((passwordOk)=>{
+                if(passwordOk){
+                    delete userInDB.password;
+                    req.session.logedUser = userInDB;
+                    console.log(req.session.logedUser);
+                    if(req.body.recordame){
+                        res.cookie('userEmail', req.body.email, {maxAge: (1000*60)*60})
+                    }
+                    res.redirect('/users/profile');
+                } else {
+                    res.render('login', {
+                                        errors: {
+                                            email: {
+                                                msg: 'El correo electrónico o constraseña son inválidas'
+                                            }
+                                        },
+                                        oldData: req.body
+                                    });
                 }
-                res.redirect('/users/profile');
-            } else {
-                res.render('login', {
-                                    errors: {
-                                        email: {
-                                            msg: 'El correo electrónico o constraseña son inválidas'
-                                        }
-                                    },
-                                    oldData: req.body
-                                });
-            }
+            });
+            
         }
     }
     // ,
