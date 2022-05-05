b038aa3e9f1e0cde12fa6b45ec974d5c69c6171a
GregoHouse <gregorioromerogarzon@gmail.com>
2022-05-05 16:36:35 -0300

    CRUD-users1.0

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,8 @@
  
 const {validationResult} = require('express-validator');
-const Users = require('../models/Users');
+//const Users = require('../models/Users');
 const bcrypt = require('bcryptjs/dist/bcrypt');
-const session = require('express-session');
+const db = require('../database/models');
  
 
 const usersController = {
@@ -25,82 +25,107 @@ const usersController = {
         const validation = validationResult(req);
         let password = req.body.password;
         let confirmPassword = req.body.confirmPassword;
-        let userInDB = Users.findByField('email',req.body.email)
-
+        //let userInDB = Users.findByField('email',req.body.email)
+        
+        
         if(validation.errors.length > 0){
             res.render('register',{
                 errors: validation.mapped(),
                 oldData: req.body
             })
-        } else if ((password && confirmPassword != undefined) && password != confirmPassword) {
-            res.render('register',{
-                password,
-                confirmPassword,
-                noSamePassmsg: 'Las contraseñas ingresadas no coinciden!',
-                oldData: req.body
-            })
-        } else if (userInDB) {
-            res.render('register',{
-                errors: {
-                    email:{
-                        msg: 'Este email ya esta registrado'
-                    }
-                },
-                oldData: req.body
-            })
-        } else {
-            let userData = req.body;
-            let userFile = req.file.filename;
-            let userToCreate = {
-                ...userData,
-                userFile
+        } 
+        
+        db.User
+        .findOne({where:{email: req.body.email}})
+        .then(userInDB =>{
+            if ((password && confirmPassword != undefined) && password != confirmPassword) {
+                res.render('register',{
+                    password,
+                    confirmPassword,
+                    noSamePassmsg: 'Las contraseñas ingresadas no coinciden!',
+                    oldData: req.body
+                })
+                .catch(err =>{res.send(err)});
+            } else if (userInDB) {
+                res.render('register',{
+                    errors: {
+                        email:{
+                            msg: 'Este email ya esta registrado'
+                        }
+                    },
+                    oldData: req.body
+                })
+                .catch(err =>{res.send(err)});
+            } else {
+
+                let userToCreate = req.body
+				const passwordHashed = bcrypt.hashSync(req.body.password,10)
+				userToCreate.password = passwordHashed
+				delete userToCreate.contrasenaConfirmacion
+
+				db.User
+				.create({
+					...userToCreate,
+			        userfile: req.file.filename 
+				})
+				.then(()=>{
+					res.redirect('/users/login');
+				})
+				.catch(err =>{res.send(err)});
             }
-            let userCreated = Users.create(userToCreate);
-            res.redirect('/users/login')
-        }
+        })
     }, 
     login: (req, res)=>{
         const validation = validationResult(req);
-        let userInDB = Users.findByField('email',req.body.email)
+        //let userInDB = Users.findByField('email',req.body.email)
       
         if(validation.errors.length > 0){ //en el caso de errores
 			res.render('login', {
 				 errors:validation.mapped(),
 				 oldData: req.body});
 
-		} else if (!userInDB) { //en el caso de que user no exista
-			res.render('login',{
-				errors: {
-					email:{
-						msg:'El correo electrónico o constraseña son inválidas'
-					}
-				}
-			})
-		} else { // No hay errores y ya verificamos que el usuario exista. Comparamos la password.
-            bcrypt.compare(req.body.password, userInDB.password)
-            .then((passwordOk)=>{
-                console.log(passwordOk);
-                if(passwordOk){
-                    delete userInDB.password;
-                    req.session.logedUser = userInDB;
-                    console.log(req.session.logedUser);
-                    if(req.body.recordame){
-                        res.cookie('userEmail', req.body.email, {maxAge: (1000*60)*60})
+		} 
+        
+        db.User
+        .findOne({where: {email: req.body.email}})
+        .then(userInDB =>{
+            if (!userInDB) { //en el caso de que user no exista
+                res.render('login',{
+                    errors: {
+                        email:{
+                            msg:'El correo electrónico o constraseña son inválidas'
+                        }
                     }
-                    res.redirect('/users/profile');
-                } else {
-                    res.render('login', {
-                                        errors: {
-                                            email: {
-                                                msg: 'El correo electrónico o constraseña son inválidas'
-                                            }
-                                        },
-                                        oldData: req.body
-                                    });
-                }
-            });
-            
-        }
+                })
+            } else { // No hay errores y ya verificamos que el usuario exista. Comparamos la password.
+                console.log(bcrypt.compareSync(req.body.password, userInDB.password));
+                bcrypt.compare(req.body.password, userInDB.password)
+                .then((passwordOk)=>{
+                    console.log(req.body.password);
+                    console.log(userInDB.password);
+                    console.log(passwordOk);
+                    if(passwordOk){
+                        delete userInDB.password;
+                        req.session.logedUser = userInDB;
+                        console.log(req.session.logedUser);
+                        if(req.body.recordame){
+                            res.cookie('userEmail', req.body.email, {maxAge: (1000*60)*60})
+                        }
+                        res.redirect('/users/profile');
+                    } else {
+                        res.render('login', {
+                                            errors: {
+                                                email: {
+                                                    msg: 'El correo electrónico o constraseña son inválidas'
+                                                }
+                                            },
+                                            oldData: req.body
+                                        });
+                    }
+                });
+                
+            }    
+        })
     }
     // ,
     // users: (req, res) =>{
