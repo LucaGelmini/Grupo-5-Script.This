737b5871e163f6ebbfba1c7d860ac6ff6f348430
Luca <lucagelmini@github.com>
2022-05-23 19:33:59 -0300

    pequeños cambios en header y mainHeader.js

--- public/js/mainHeader.js
+++ public/js/mainHeader.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
     //dom is fully loaded, but maybe waiting on images & css files
     
-console.log('js cargadooooo')
+    console.log('js cargadooooo')
 
-    searchButton = document.getElementById('headerSearchButton');
-    searchForm = document.getElementById('headerSearchForm');
-
+    let searchButton = document.getElementById('headerSearchButton');
+    let searchForm = document.getElementById('headerSearchForm');
+    let burgerButton = document.querySelector('.burger-container');
+    let navsContainer = document.querySelector('.navs-container');
 
     searchButton.addEventListener('click', function(){
         searchForm.submit();
-    })
+    });
 
     searchForm.onkeydown = e => {
-        e.keyCode == 13 ? searchForm.submit : null;
+        e.key === "Enter" ? searchForm.submit : null;
+    };
+
+    burgerButton.onclick = ()=> {
+        navsContainer.classList.toggle('navs-container-closed');
+        navsContainer.classList.toggle('navs-container-open');
+        
     }
     //Este comentario es para ver si se mergea bien esto
 });
