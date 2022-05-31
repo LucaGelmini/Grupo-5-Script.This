41349a229b326e32b6aaa75a254a9bbf3b4eae54
David Santiago Benjumea Perez <davidbenjumchess@gmail.com>
2022-05-30 19:42:14 -0500

    fixes

--- src/middlewares/unitsMiddleware.js
+++ src/middlewares/unitsMiddleware.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 const unitsMiddleware = [
     body('unidad').notEmpty().withMessage('Ingrese algo al formuluario antes de enviar').bail()
-                  .matches('^[a-zA-Z]*$').withMessage('No puede contener numeros ni caracteres especiales')
+                  .matches('^[a-zA-Z]*$').withMessage('No puede contener números ni caracteres especiales')
                   
                 
 ]
