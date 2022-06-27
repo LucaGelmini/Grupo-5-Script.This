86bde717494dc65265d651d52214a8d400368dce
Luca <lucagelmini@github.com>
2022-06-27 13:46:45 -0300

    2

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,5 +5,17 @@ module.exports = {
     "database": "bolsonverde_db",
     "host": "ec2-44-206-89-185.compute-1.amazonaws.com",
     "dialect": "postgresql"
+  },
+  "production": {
+    "username": "twzqucbiestlkh",
+    "password": "34a56f19fa27729f7df6e20a50055625d1fbd61f574a87b96ce092ab76c46f75",
+    "database": "bolsonverde_db",
+    "host": "ec2-44-206-89-185.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
--- src/database/models/index.js
+++ src/database/models/index.js
@@ -11,7 +11,9 @@ const db = {};
 let sequelize;
 console.log('\n\n\n'+process.env.DATABASE_URL+'\n\n\n')
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: "postgres"});
+  sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: "postgres" ,dialectOptions: {
+    ssl: true
+  }});
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
